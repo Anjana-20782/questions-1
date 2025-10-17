6ebc927d7ad39f9e11b88d5f766e38c6d3905c2b
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-17 15:12:37 +0530

    new contents added

--- js/Questions/palindrome.js
+++ js/Questions/palindrome.js
@@ -1,15 +1,28 @@
 let str="ardra";
-let rev=""
-for(i=str.length-1;i>=0;i--)
-{
-     rev += str[i];
-}
-if(rev==str){
+// let rev=""
+// for(i=str.length-1;i>=0;i--)
+// {
+//      rev += str[i];
+// }
+// if(rev==str){
 
-    console.log(str+" is palidrome");
+//     console.log(str+" is palidrome");
     
-}
-else{
-    console.log(str+" not palidrome");
+// }
+// else{
+//     console.log(str+" not palidrome");
     
-}
+// }
+
+                //OR
+                
+let rev=str.split(" ").reverse().join(" ")
+ if(rev==str){
+
+     console.log(str+" is palidrome");
+    
+ }
+ else{
+     console.log(str+" not palidrome");
+    
+ }
